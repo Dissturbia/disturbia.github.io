dc4c2b3297bacf2d4ffd82fc9a4eeadf1098a5e2
Gise Ramirez <35972225+Dissturbia@users.noreply.github.com>
2019-10-12 21:20:08 -0300

    problema sin solucionar

--- proyect-ph/src/App.js
+++ proyect-ph/src/App.js
@@ -8,7 +8,7 @@ function App() {
   return (
     <div style={{height: '300px', position: 'relative'}}>
     <Layout fixedHeader>
-        <Header title={<span><span style={{ color: '#ddd' }}>Area / </span><strong>The Title</strong></span>}>
+        <Header className = "header-color" title = "Looking for the sunshine" scroll>
             <Navigation>
                 <Link to ="/resume">Resume</Link>
                 <Link to ="/aboutme">About Me</Link>
--- proyect-ph/src/components/landingpage.js
+++ proyect-ph/src/components/landingpage.js
@@ -1,12 +1,26 @@
 import React , { Component } from 'react';
+import {Cell , Grid } from 'react-mdl';
 
 
 class Landing extends Component {
     render () {
         return (
-            <div><h1> Landing page</h1></div>
-        )
+            <div style={{width:'100%' , margin:'auto'}}>
+                <Grid className="landing-grid">
+                    <Cell col={12}>
+                       <img 
+                         src="https://i0.pngocean.com/files/449/984/899/camera-drawing-sketch-camera-drawing.jpg"
+                         alt="avatar" 
+                         className="avatar-img"
+                         />
+                        
+                    </Cell>
 
+                </Grid>
+
+
+            </div>
+        )
         
     }
 }
